00cf7288ba1231f36f7ab05475feff9caffd76ad
Stelios Manousopoulos <smanousopoulos@gmail.com>
2017-05-09 00:15:54 +0300

    updated translations with english translations

--- src/i18n/es.js
+++ src/i18n/es.js
@@ -1,9 +1,56 @@
 export default {
   "report": {
     "title": "Monthly water report",
-    "subtitle": "Want detailed access to your consumption? Sign up for free at",
+    "subtitleL1": "Want detailed access to your consumption?",
+    "subtitleL2": "Sign up for free at", 
     "dear": "Dear {firstname},",
-    "textP1": "This is your personal monthly water report for the period: {from}-{to}. This not a bill, but an analysis of your water use for this month to help you understand how you use and how you can save water! ",
+    "textP1": "This is your personal monthly water report for {period}. This not a bill, but an analysis of your water use for this month to help you understand how you use and how you can save water! ",
     "textP2": "We, at {utility}, strive to make water available to everyone and safeguard our precious water resources. You can help in this quest by using water more efficiently, where and when you need it."
+  },
+  "widgets": {
+    "titles": {
+      "totalVolumeStatAmphiro": "Shower Volume (metric)",
+      "totalVolumeChartAmphiro": "Shower Volume (progress)",
+      "totalVolumeStatSWM": "Water (metric)",
+      "totalVolumeChartSWM": "Water (progress)",
+      "totalEnergyStat": "Shower Energy (metric)",
+      "totalEnergyChart": "Shower Energy (progress)",
+      "totalTemperatureStat": "Shower Temperature (metric)",
+      "totalTemperatureChart": "Shower Temperature (progress)",
+      "last": "Last shower",
+      "efficiencyEnergy": "Energy efficiency",
+      "ranking": "Shower ranking",
+      "breakdown": "Water breakdown",
+      "forecast": "Forecast - next month",
+      "pricing": "Pricing - this month",
+      "comparison": "Comparison",
+      "budget": "Monthly budget",
+      "wateriqStat": "Water IQ",
+      "wateriqChart": "Water IQ Comparison",
+      "commons": "Community comparison",
+      "tip": "Tip"
+    },
+    "descriptions": {
+      "totalVolumeStatAmphiro": "The total consumption for your last 100 showers.",
+      "totalVolumeChartAmphiro": "The consumption for your last 100 showers for {device}.",
+      "totalVolumeStatSWM": "Your household's total water consumption.",
+      "totalVolumeChartSWM": "Your household's water consumption progress.",
+      "totalEnergyStat": "The total energy consumption for your last 100 showers.", 
+      "totalEnergyChart": "The energy progress for your last 100 showers.", 
+      "totalTemperatureStat": "The average temperature for your last 100 showers.",
+      "totalTemperatureChart": "The average temperature variation for your last 100 showers.",
+      "last": "The last shower recorded for all your devices.",
+      "efficiencyEnergy": "Your shower energy score for the last 100 showers.",
+      "ranking": "The household members' shower ranking.",
+      "breakdown": "Your computed water use per household appliance.", 
+      "forecast": "Our estimations for your water use for the next month.",
+      "pricing": "Your monthly consumption within the existing price ranges",
+      "comparison": "Some useful comparisons based on your water consumption.",
+      "budget": "Your consumption based on your daily budget.",
+      "wateriqStat": "Your Water IQ score for the previous month.",
+      "wateriqChart": "Some useful comparisons based on your water IQ score.",
+      "commons": "Your consumption against your favorite community.",
+      "tip": "A tip to help you reduce consumption"
+    }
   }
 };
